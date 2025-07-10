cc05472ff863722c18b8a0bac70a5ad6d267a008
Stephane <boluge@gmail.com>
2025-07-10 09:01:39 +0200

    feat: add icons

---
+++ client/static/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'mon-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/*.css',
+  '/*.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne la ressource du cache si disponible
+        if (response) {
+          return response;
+        }
+        // Sinon, fait la requête réseau
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activation du service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
